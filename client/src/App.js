fe2848da811dfec0c0f836db5ea659e9910b1f53
Helio <sinjjangga@gmail.com>
2021-09-26 16:31:09 +0900

    서버 고객 추가 모듈 추가 및 화면 리플레쉬 기능 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import CustomerAdd from './components/CustomerAdd';
 import Customer from './components/Customer';
 import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
@@ -34,10 +35,24 @@ const styles = theme => ({
 
 class App extends Component {
 
-  state = {
-    customers: "",
-    completed: 0
+  constructor(props){
+    super(props);
+    this.state = {
+      customers: '',
+      completed: 0
+    }
   }
+
+  stateRefresh = () => {
+    this.setState({
+      customers: '',
+      completed: 0
+    }); 
+    this.callApi()
+    .then(res => this.setState({customers: res}))
+    .catch(err => console.log(err));
+  }
+
   //컴포넌트 마운트가 끝난 이후 호출되는 내장함수
   componentDidMount() {
     this.timer = setInterval(this.progress, 50);
@@ -62,43 +77,46 @@ class App extends Component {
   render(){
     const { classes } = this.props;
     return(
-      <Paper className={classes.root}>
-        <Table className={classes.table}>
-          <TableHead>
-            <TableRow>
-              <TableCell>번호</TableCell>
-              <TableCell>이미지</TableCell>
-              <TableCell>이름</TableCell>
-              <TableCell>생년월일</TableCell>
-              <TableCell>성별</TableCell>
-              <TableCell>직업</TableCell>
-            </TableRow>
-          </TableHead>
-
-          <TableBody>
-              {this.state.customers ? this.state.customers.map(c => {
-                return(
-                  <Customer
-                    key = {c.id}
-                    id = {c.id}
-                    image = {c.image}
-                    name = {c.name}
-                    birthday = {c.birthday}
-                    gender = {c.gender}
-                    job = {c.job}
-                  / >
-                );
-              }) : 
+      <>
+        <Paper className={classes.root}>
+          <Table className={classes.table}>
+            <TableHead>
               <TableRow>
-                <TableCell colSpan="6" align="center">
-                  <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed} />
-                </TableCell>
+                <TableCell>번호</TableCell>
+                <TableCell>이미지</TableCell>
+                <TableCell>이름</TableCell>
+                <TableCell>생년월일</TableCell>
+                <TableCell>성별</TableCell>
+                <TableCell>직업</TableCell>
               </TableRow>
-            }
-          </TableBody>
-        </Table>
-        <Button variant="">Yes!!</Button>
-      </Paper>
+            </TableHead>
+
+            <TableBody>
+                {this.state.customers ? this.state.customers.map(c => {
+                  return(
+                    <Customer
+                      key = {c.id}
+                      id = {c.id}
+                      image = {c.image}
+                      name = {c.name}
+                      birthday = {c.birthday}
+                      gender = {c.gender}
+                      job = {c.job}
+                    / >
+                  );
+                }) : 
+                <TableRow>
+                  <TableCell colSpan="6" align="center">
+                    <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed} />
+                  </TableCell>
+                </TableRow>
+              }
+            </TableBody>
+          </Table>
+          <Button variant="">Yes!!</Button>
+        </Paper>
+        <CustomerAdd stateRefresh={this.stateRefresh} />
+      </>
     );
   }
 }
--- server.js
+++ server.js
@@ -10,33 +10,54 @@ app.use(bodyParser.urlencoded({ extended: true}));
 //     res.send({message: 'Hello Express!'});
 // });
 
+const multer = require('multer');
+const upload = multer({desc: './upload'});
+
+const customers = [
+    {
+        'id': 1,
+        'image': 'https://placeimg.com/64/64/1',
+        'name': '홍길동',
+        'birthday': '921212',
+        'gender': 'male',
+        'job': 'student'
+    },
+    {
+        'id': 2,
+        'image': 'https://placeimg.com/64/64/2',
+        'name': '조자룡1',
+        'birthday': '910102',
+        'gender': 'male',
+        'job': 'student'
+    },
+    {
+        'id': 3,
+        'image': 'https://placeimg.com/64/64/3',
+        'name': '관우1',
+        'birthday': '900321',
+        'gender': 'male',
+        'job': 'student'
+    }
+];
+
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-            'id': 1,
-            'image': 'https://placeimg.com/64/64/1',
-            'name': '홍길동',
-            'birthday': '921212',
-            'gender': 'male',
-            'job': 'student'
-        },
-        {
-            'id': 2,
-            'image': 'https://placeimg.com/64/64/2',
-            'name': '조자룡',
-            'birthday': '910102',
-            'gender': 'male',
-            'job': 'student'
-        },
-        {
-            'id': 3,
-            'image': 'https://placeimg.com/64/64/3',
-            'name': '관우',
-            'birthday': '900321',
-            'gender': 'male',
-            'job': 'student'
-        }
-    ]);
+    res.send(customers);
+    console.log('get api');
 });
+
+app.use('/image', express.static('./upload')); 
+app.post('/api/customers', upload.single('image'), (req, res) => {
+    let maxId = customers[customers.length - 1].id + 1;
+    console.log(maxId);
+    customers.push({
+        'id': maxId,
+        'image': 'https://placeimg.com/64/64/'+maxId,
+        'name': req.body.name,
+        'birthday': req.body.birthday,
+        'gender': req.body.gender,
+        'job': req.body.job
+    });
+    res.send(customers);
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
